423e1f8380255a8d426cb9fd2e2ce28dd763717e
709248556 <709248556@qq.com>
2020-01-09 17:10:35 +0800

    EchoClent

--- chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
+++ chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
@@ -32,6 +32,8 @@ public class EchoClient {
             b.group(group)
                 .channel(NioSocketChannel.class)
                 .remoteAddress(new InetSocketAddress(host, port))
+                    //当一个新的连接被接受时，一个新的子 Channel 将会被创建，
+                    // 而 ChannelInitializer 将会把一个你的EchoServerHandler的实例添加到该Channel的ChannelPipeline中
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     public void initChannel(SocketChannel ch)
@@ -40,10 +42,10 @@ public class EchoClient {
                              new EchoClientHandler());
                     }
                 });
-            ChannelFuture f = b.connect().sync();
-            f.channel().closeFuture().sync();
+            ChannelFuture f = b.connect().sync();//异步地绑定服务器调用 sync()方法阻塞等待直到绑定完成
+            f.channel().closeFuture().sync();//获取Channel的CloseFuture，并且阻塞当前线程直到它完成
         } finally {
-            group.shutdownGracefully().sync();
+            group.shutdownGracefully().sync();//关闭，释放资源
         }
     }
 
